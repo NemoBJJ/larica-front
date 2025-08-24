7f14b4429d88d5543a9a51a5c923dcdcf59980b7
NemoBJJ <engnfaraujo@gmail.com>
2025-08-24 11:41:11 -0300

    DEPLOY PORRA

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,7 +4,7 @@ const BASE =
   (typeof window !== 'undefined' && (window as any).__API_BASE_URL__) ||
   (typeof import.meta !== 'undefined' && (import.meta as any).env?.VITE_API_BASE_URL) ||
   process.env.REACT_APP_API_BASE_URL ||
-  'http://localhost:8086/api';
+  'https://api-larica.neemindev.com/api'; // ✅ ADD https:// e /api
 
 const api = axios.create({
   baseURL: BASE,
